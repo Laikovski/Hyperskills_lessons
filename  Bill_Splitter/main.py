f0a92cba780119cadc4dd6785b25db0147a71add
laikov <zheka-se@ya.ru>
2021-06-02 19:18:27 +0300

    02-06-final

---  Bill_Splitter/main.py
+++  Bill_Splitter/main.py
@@ -1,34 +1,34 @@
 # Stage 1/4: Invite your friends
-# print('Enter the number of friends joining (including you):')
-# count_friends = int(input())
-# print('')
-# dict_friends = {}
-# if count_friends > 0:
-#     print('Enter the name of every friend (including you), each on a new line:')
-#     for i in range(count_friends):
-#         dict_friends[input()] = 0
-#     print('')
-#     print(dict_friends)
-# else:
-#     print('No one is joining for the party')
+print('Enter the number of friends joining (including you):')
+count_friends = int(input())
+print('')
+dict_friends = {}
+if count_friends > 0:
+    print('Enter the name of every friend (including you), each on a new line:')
+    for i in range(count_friends):
+        dict_friends[input()] = 0
+    print('')
+    print(dict_friends)
+else:
+    print('No one is joining for the party')
 
 # Stage 2/4: The bill has arrived
 
-# print('Enter the number of friends joining (including you):')
-# count_friends = int(input())
-# print('')
-# dict_friends = {}
-# if count_friends > 0:
-#     print('Enter the name of every friend (including you), each on a new line:')
-#     for i in range(count_friends):
-#         dict_friends[input()] = 0
-#     print('\nEnter the total bill value:')
-#     sum_bill = round(int(input()) / count_friends, 2)
-#     for k, v in dict_friends.items():
-#         dict_friends[k] = sum_bill
-#     print(dict_friends)
-# else:
-#     print('No one is joining for the party')
+print('Enter the number of friends joining (including you):')
+count_friends = int(input())
+print('')
+dict_friends = {}
+if count_friends > 0:
+    print('Enter the name of every friend (including you), each on a new line:')
+    for i in range(count_friends):
+        dict_friends[input()] = 0
+    print('\nEnter the total bill value:')
+    sum_bill = round(int(input()) / count_friends, 2)
+    for k, v in dict_friends.items():
+        dict_friends[k] = sum_bill
+    print(dict_friends)
+else:
+    print('No one is joining for the party')
 
 # Stage 3/4: The Lucky One
 import random
@@ -54,5 +54,39 @@ if count_friends > 0:
     # for k, v in dict_friends.items():
     #     dict_friends[k] = sum_bill
     # print(dict_friends)
+else:
+    print('No one is joining for the party')
+
+# Stage 4/4: Party is over
+
+import random
+random_friend = ''
+print('Enter the number of friends joining (including you):')
+count_friends = int(input())
+print('')
+dict_friends = {}
+if count_friends > 0:
+    print('Enter the name of every friend (including you), each on a new line:')
+    for i in range(count_friends):
+        dict_friends[input()] = 0
+
+    print('\nEnter the total bill value:')
+    sum_bill = int(input())
+
+    print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+    result = input().lower()
+    if result == 'yes':
+        random_friend = random.choice(list(dict_friends.keys()))
+        print(f'{random_friend} is the lucky one!')
+        sum_bill = sum_bill / (count_friends - 1)
+    else:
+        print('No one is going to be lucky')
+        sum_bill = sum_bill / count_friends
+    for k, v in dict_friends.items():
+        if result == 'yes' and random_friend == k:
+            dict_friends[k] = 0
+        else:
+            dict_friends[k] = round(sum_bill, 2)
+    print(dict_friends)
 else:
     print('No one is joining for the party')
